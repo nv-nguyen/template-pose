0427f223d2c10dfd5bf9505494ad281166149f39
nv-nguyen <vanngn.nguyen@gmail.com>
2023-05-16 23:08:57 +0200

    Remove wandb in network

--- src/model/base_network.py
+++ src/model/base_network.py
@@ -6,7 +6,6 @@ import torch.nn.functional as F
 from src.utils.visualization_utils import put_image_to_grid
 from torchvision.utils import make_grid, save_image
 import os
-import wandb
 import torchvision.transforms as transforms
 from src.model.loss import GeodesicError
 import multiprocessing
